8dbb18e9036d267dca8efa88648a78e0ddee1558
harley-travis <harley.travis2@gmail.com>
2019-11-09 15:33:30 -0700

    forgot one page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
+            <div class="card shadow">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
