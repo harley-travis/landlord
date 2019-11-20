b1f8776b3dc6eb01dff31ccc8273edd3db9069e1
harley-travis <harley.travis2@gmail.com>
2019-11-20 01:01:13 -0700

    updated mail driver

--- config/mail.php
+++ config/mail.php
@@ -133,4 +133,10 @@ return [
 
     'log_channel' => env('MAIL_LOG_CHANNEL'),
 
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+
 ];
