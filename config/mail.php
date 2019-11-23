e4c3daca51cbf45f75126af4b22ab45d7c5f0270
harley-travis <harley.travis2@gmail.com>
2019-11-22 17:01:07 -0700

    fixing some custom features for mailing

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@senrent.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@senrent.com'),
+        'name' => env('MAIL_FROM_NAME', 'Welcome to SenRent'),
     ],
 
     /*
