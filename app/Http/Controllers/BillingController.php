3382395247f2314de4b264481ac5899f650c7801
harley-travis <harley.travis2@gmail.com>
2019-12-03 22:44:19 -0700

    getting real tired of this

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -1013,7 +1013,7 @@ class BillingController extends Controller {
             'source' => $bank_account, 
             'customer' => $customer->id,
             'transfer_data' => [
-                'amount' => ( $total - $convenience ) * 100, 
+                'amount' => $total - $convenience, 
                 'destination' => $proprietor->stripe_account, 
             ],
         ]);
