10b3c1cf65be2556597dd81642603a5319d9ce54
harley-travis <harley.travis2@gmail.com>
2019-12-16 23:51:44 -0700

    work work work'

--- resources/views/settings/billing/index.blade.php
+++ resources/views/settings/billing/index.blade.php
@@ -121,7 +121,7 @@
                     <ul class="list-group">
 						<li class="list-group-item">
 							<span class="col-6"> 
-                                <i class="fas fa-university mr-2"></i> {{ $connect_accounts }} 
+                                <i class="fas fa-university mr-2"></i> {{ $connect_accounts->external_accounts->data->bank_name }} 
                                 <span class="pl-3">********  </span>
                             
      
