d6cacb3e4f5005d0e0ed5cddcb94d086ce087a39
Travis Harley <harley.travis2@gmail.com>
2019-12-16 23:36:52 -0700

    echo data

--- resources/views/settings/billing/index.blade.php
+++ resources/views/settings/billing/index.blade.php
@@ -126,7 +126,7 @@
 					    @foreach( $bank_accounts as $bank_account )
 						<li class="list-group-item">
 							<span class="col-6"> 
-                                <i class="fas fa-university mr-2"></i> @if( !isset($bank_account->bank_name) ) {{ $bank_account->external_accounts->data }} @else {{ $bank_account->bank_name }} @endif
+                                <i class="fas fa-university mr-2"></i> @if( !isset($bank_account->bank_name) ) {{ $bank_account }} @else {{ $bank_account->bank_name }} @endif
                                 <span class="pl-3">******** @if( !isset($bank_account->last4) ) {{ $bank_account->external_accounts->data->last4 }} @else {{ $bank_account->last4 }} @endif </span>
                             
                                 @if($bank_account->id == $customer->default_source)
