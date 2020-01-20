5d9b4355df77e1580a61401c20711df50a5dc4d7
harley-travis <harley.travis2@gmail.com>
2020-01-20 14:35:29 -0700

    playing with the settings to see where i am with the router problem

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
 	
 	if( auth()->check() == null ) {
@@ -41,6 +30,8 @@ Route::get('/', function () {
         // } else {
         //   return view('dashboard');
         // }
+
+        return view('dashboard');
 
         break;
       case 4:
