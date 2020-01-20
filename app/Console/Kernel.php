7467d285e40e5a400c50262d396777d649866659
harley-travis <harley.travis2@gmail.com>
2020-01-20 15:41:06 -0700

    changed command to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('db:backup')->everyMinute();
+        $schedule->command('db:backup')->daily();
     }
 
     /**
