f874f6269a9c9c1ec8aff8c1a7146faacf8b7998
harley-travis <harley.travis2@gmail.com>
2020-01-27 14:37:09 -0700

    changed the reports icon to green and updated the wording in the email

--- resources/views/emails/tenant/created.blade.php
+++ resources/views/emails/tenant/created.blade.php
@@ -3,10 +3,14 @@
 
 Your property owner just created an account for you at SenRent.com. Login to start paying rent online.
 
-Click here to reset your password.
+In order to get started, first reset your password, then verify your email address. 
+
+Click the button below to reset your password. 
+
+After you have reset your password, please verify your email account. 
 
 @component('mail::button', ['url' => 'https://app.senrent.com/password/reset'])
-Login
+Set Password
 @endcomponent
 
 @endcomponent
