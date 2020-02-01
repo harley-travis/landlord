7e3e06d0e8aa756a16e7e6fbd553fccf641340be
harley-travis <harley.travis2@gmail.com>
2020-01-31 22:43:22 -0700

    updated email confirmation to work for both paid online and with cash/check

--- resources/views/emails/payment/confirmation.blade.php
+++ resources/views/emails/payment/confirmation.blade.php
@@ -1,7 +1,9 @@
 @component('mail::message')
 # Thank you {{ $user->name }} for your payment,
 
-Your payment for ${{ number_format( $total / 100,2 ) }} was sucessful. The funds will be withdrawn from your account in 1-3 business days. If you have any questions, please let us know.
+Your payment for ${{ number_format( $total ) }} was sucessful. The funds will be withdrawn from your account in 1-3 business days. If you paid by cash or check, then no other action is needed.
+
+If you have any questions, please let us know.
 
 Thank you!<br>
 
